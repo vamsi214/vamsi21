1b20b750fafac7f45bf1f65ec2afae6e2cb3b5ea
vamsi214 <30810657+vamsi214@users.noreply.github.com>
2017-08-08 01:46:28 +0530

    Add files via upload

---
+++ mysite2/polls/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import Http404
+
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index2.html', context)
+
+def detail(request, question_id):
+	try:	
+		question=Question.objects.get(pk=question_id)
+	except:
+		raise Http404("Sorry page was not found")
+	return render(request,'polls/index.html',{'question':question})
+
+def detail2(request, question_id):
+	try:	
+		question=Question.objects.get(pk=question_id)
+	except:
+		raise Http404("Sorry page was not found")
+	return render(request,'polls/index3.html',{'question':question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+    
